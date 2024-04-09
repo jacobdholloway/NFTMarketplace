aad497883213ad32d036a742da1d95b990bc6e56
Jacob Holloway <jholloway1342@sdsu.edu>
2024-04-09 15:13:04 -0700

    updated settings,url,views,templates

--- marketPlace/marketPlace/urls.py
+++ marketPlace/marketPlace/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from marketPlace import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.home, name='home'),
 ]
---
+++ marketPlace/marketPlace/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'marketPlace/test.html', {'page_title': 'Home Page'})
