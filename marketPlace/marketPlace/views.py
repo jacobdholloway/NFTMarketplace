2f7492c06f31258a38267e3cce6c237edd0f9810
Jacob Holloway <jholloway1342@sdsu.edu>
2024-04-10 11:54:18 -0700

    added gitignore

--- marketPlace/marketPlace/views.py
+++ marketPlace/marketPlace/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'marketPlace/test.html', {'page_title': 'Home Page'})
+    return render(request, 'marketPlace/test.html', {'page_title': 'NFT Marketplace'})
