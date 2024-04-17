0d9092c56b261976067fa559fc9964b294232737
brandonlipscomb <108440960+brandonlipscomb@users.noreply.github.com>
2024-04-16 17:20:51 -0700

    Update MyNFT.sol
    Added variable to keep track of who deployed the contract. Only allow that person to mint NFTs.

--- marketPlace/NFTProject/contracts/MyNFT.sol
+++ marketPlace/NFTProject/contracts/MyNFT.sol
@@ -5,12 +5,22 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721URIStorage, Ownable {
-    uint256 private _currentTokenId = 0;
 
-    constructor() ERC721("MyNFT", "MNFT") Ownable(msg.sender) {}
+    uint256 private _currentTokenId = 0;
+    address private _deployer;
+    
+    constructor() ERC721("MyNFT", "MNFT") Ownable() {
+        _deployer = msg.sender;
+    }
+
+    modifier onlyDeployer() {
+        require(msg.sender == _deployer, "Only deployer can mint tokens");
+        _;
+    }
 
     // Function to mint a new NFT
-    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
+    // Changed from onlyOwner to onlyDeployer 
+    function mintNFT(address recipient, string memory tokenURI) public onlyDeployer returns (uint256) {
         uint256 newTokenId = _currentTokenId++;
         _mint(recipient, newTokenId);
         _setTokenURI(newTokenId, tokenURI);
