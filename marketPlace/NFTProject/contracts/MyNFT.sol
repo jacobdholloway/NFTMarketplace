d0da060991c39cbc845123f7578a36a3d21e67c6
brandonlipscomb <108440960+brandonlipscomb@users.noreply.github.com>
2024-05-06 21:36:20 -0700

    Update MyNFT.sol
    Added URI functionality to the MyNFT.sol contract

--- marketPlace/NFTProject/contracts/MyNFT.sol
+++ marketPlace/NFTProject/contracts/MyNFT.sol
@@ -1,29 +1,42 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+contract MyNFT is ERC721 {
+    using Strings for uint256;
 
-    uint256 private _currentTokenId = 0;
     address private _deployer;
-    
-    constructor() ERC721("MyNFT", "MNFT") Ownable() {
+    string private _baseURIextended;
+    mapping(uint256 => bool) private _tokenExists;
+
+    constructor(string memory baseURI) ERC721("MyNFT", "MNFT") {
         _deployer = msg.sender;
+        _baseURIextended = baseURI;
     }
 
-    modifier onlyDeployer() {
-        require(msg.sender == _deployer, "Only deployer can mint tokens");
-        _;
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseURIextended;
     }
 
-    // Function to mint a new NFT
-    // Changed from onlyOwner to onlyDeployer 
-    function mintNFT(address recipient, string memory tokenURI) public onlyDeployer returns (uint256) {
-        uint256 newTokenId = _currentTokenId++;
-        _mint(recipient, newTokenId);
-        _setTokenURI(newTokenId, tokenURI);
-        return newTokenId;
+    // Corrected: removed the override keyword
+    function _exists(uint256 tokenId) internal view returns (bool) {
+        return _tokenExists[tokenId];
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 
+            ? string(abi.encodePacked(baseURI, "NFT", tokenId.toString(), ".json"))
+            : "";
+    }
+
+    function mint(address to, uint256 tokenId) public {
+        require(msg.sender == _deployer, "Only deployer can mint");
+        require(!_exists(tokenId), "ERC721: token already minted"); // Ensure token doesn't already exist
+        _tokenExists[tokenId] = true;  // Mark the token as exists to prevent re-minting
+        _mint(to, tokenId);  // Call the internal _mint function
     }
 }
